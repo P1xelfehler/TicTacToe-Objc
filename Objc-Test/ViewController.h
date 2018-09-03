268b31e535c2dd7d350aede9469f0ee0a1df3c2d
P1xelfehler <norman.laudien1996@gmail.com>
2018-09-03 15:43:14 +0200

    added play again button & prepared evaluation

--- Objc-Test/ViewController.h
+++ Objc-Test/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *playAgainButton;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *fieldButtons;
 
 @end
 
--- Objc-Test/ViewController.m
+++ Objc-Test/ViewController.m
@@ -17,10 +17,14 @@
 short player = 1;
 NSMutableArray *state;
 
+// MARK: - Lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self startGame];
 }
+
+// MARK: - Actions
 
 - (IBAction)fieldTapped:(UIButton *)sender {
     if ([sender titleForState:UIControlStateNormal] != nil) {
@@ -35,11 +39,38 @@ NSMutableArray *state;
         [sender setTitle:@"O" forState:UIControlStateNormal];
         player = 1;
     }
-    NSLog(@"%@", state);
+    [self evaluate];
 }
+
+- (IBAction)playAgainButtonTapped:(UIButton *)sender {
+    for (UIButton *button in _fieldButtons) {
+        [button setTitle:nil forState:UIControlStateNormal];
+    }
+    [self startGame];
+}
+
+// MARK: - Functions
 
 -(void)startGame {
     state = [[NSMutableArray alloc] initWithObjects:@0,@0,@0,@0,@0,@0,@0,@0,@0, nil];
+    [[self playAgainButton] setHidden:true];
+}
+
+-(void)evaluate {
+    if ([self checkIfFinished]) { // finished
+        [[self playAgainButton] setHidden:false];
+        return;
+    }
+}
+
+-(BOOL)checkIfFinished {
+    NSLog(@"%@\n", state);
+    for (NSNumber *item in state) {
+        if ([item isEqualToNumber:[NSNumber numberWithInteger:0]]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 @end
