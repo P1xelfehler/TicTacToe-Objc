2cc26baffe15048f6b4d2dcd9bc56ddef76e9712
P1xelfehler <norman.laudien1996@gmail.com>
2018-09-03 13:15:35 +0200

    Initial Commit

---
+++ Objc-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objc-Test
+//
+//  Created by Norman Laudien on 03.09.18.
+//  Copyright © 2018 Norman Laudien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objc-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objc-Test
+//
+//  Created by Norman Laudien on 03.09.18.
+//  Copyright © 2018 Norman Laudien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
