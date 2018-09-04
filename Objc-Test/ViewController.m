920fdc4f8a81624b4aa9d1d5fc5d00a4b8b6e870
P1xelfehler <norman.laudien1996@gmail.com>
2018-09-04 10:05:06 +0200

    finished the base game

--- Objc-Test/ViewController.m
+++ Objc-Test/ViewController.m
@@ -15,7 +15,7 @@
 @implementation ViewController
 
 short player = 1;
-short *state[9];
+short state[9];
 
 // MARK: - Lifecycle
 
@@ -31,21 +31,20 @@ short *state[9];
         return;
     }
     NSInteger tag = [sender tag];
-    state[tag] = &player;
+    state[tag] = player;
     if (player == 1) {
         [sender setTitle:@"X" forState:UIControlStateNormal];
-        player = 2;
     } else {
         [sender setTitle:@"O" forState:UIControlStateNormal];
-        player = 1;
     }
     [self evaluate];
+    player = player == 1 ? 2 : 1;
 }
 
 - (IBAction)playAgainButtonTapped:(UIButton *)sender {
     for (int i = 0; i < 9; i++) {
         [_fieldButtons[i] setTitle:NULL forState:UIControlStateNormal];
-        state[i] = NULL;
+        state[i] = 0;
     }
     [self startGame];
 }
@@ -57,19 +56,84 @@ short *state[9];
 }
 
 -(void)evaluate {
-    if ([self checkIfFinished]) { // finished
+    short winner = [self getWinner];
+    if (winner != 0) { // someone has won
         [[self playAgainButton] setHidden:false];
+        [self showWinnerAlert:winner];
+        return;
+    }
+    if ([self isFieldFull]) { // field is full
+        [[self playAgainButton] setHidden:false];
+        [self showDrawAlert];
         return;
     }
 }
 
--(BOOL)checkIfFinished {
+-(BOOL)isFieldFull {
     for (int i = 0; i < 9; i++) {
-        if (state[i] == NULL) {
+        if (state[i] == 0) {
             return false;
         }
     }
     return true;
+}
+
+-(short)getWinner {
+    short first, second, third;
+    for(int i = 0; i < 3; i++) {
+        // horizontal rows
+        first = state[i + i * 2];
+        second = state[i + i * 2 + 1];
+        third =  state[i + i * 2 + 2];
+        if (first != 0 && first == second && second == third) {
+            return first;
+        }
+        // vertical rows
+        first = state[i];
+        second = state[i + 3];
+        third =  state[i + 6];
+        if (first != 0 && first == second && second == third) {
+            return first;
+        }
+        // diagonal rows
+        if (i == 2) {
+            continue;
+        }
+        first = state[i * 2];
+        second = state[4];
+        third = state[8 - 2 * i];
+        if (first != 0 && first == second && second == third) {
+            return first;
+        }
+    }
+    return 0;
+}
+
+-(void)showWinnerAlert:(short)winner {
+    NSString *winnerLabel = winner == 1 ? @"X" : @"O";
+    UIAlertController *alertController = [UIAlertController
+                                          alertControllerWithTitle:@"Gewonnen"
+                                          message:[NSString stringWithFormat:@"%@ hat gewonnen!", winnerLabel]
+                                          preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction
+                               actionWithTitle:@"Ok"
+                               style:UIAlertActionStyleDefault
+                               handler:NULL];
+    [alertController addAction:okAction];
+    [self presentViewController:alertController animated:true completion:NULL];
+}
+
+-(void)showDrawAlert {
+    UIAlertController *alertController = [UIAlertController
+                                          alertControllerWithTitle:@"Unentschieden"
+                                          message:@"Das Spiel endet unentschieden."
+                                          preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction
+                               actionWithTitle:@"Ok"
+                               style:UIAlertActionStyleDefault
+                               handler:NULL];
+    [alertController addAction:okAction];
+    [self presentViewController:alertController animated:true completion:NULL];
 }
 
 @end
